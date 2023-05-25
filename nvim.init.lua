df483029a9e8e810e49927a3a93e7ec3515c25bc
Salil Wadnerkar <rohshall@noreply.codeberg.org>
2023-05-25 01:09:48 +0000

    Update 'nvim.init.lua'

--- nvim.init.lua
+++ nvim.init.lua
@@ -5,9 +5,22 @@ local function load_plugins()
         use 'dense-analysis/ale'
         use 'junegunn/fzf.vim'
         use {
-            'ojroques/nvim-hardline',
-            requires = {
-                bufferline = true
+            'akinsho/bufferline.nvim',
+            tag = "*",
+            requires = 'nvim-tree/nvim-web-devicons'
+        } 
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = ':TSUpdate'
+        }
+        use 'tpope/vim-eunuch'
+        use {
+            'nvim-neo-tree/neo-tree.nvim',
+            branch = "v2.x",
+            requires = { 
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+                "MunifTanjim/nui.nvim",
             }
         }
     end)
@@ -35,16 +48,26 @@ local load_config = function()
     vim.g.netrw_liststyle = 3
     vim.g.netrw_browse_split = 4
     vim.g.netrw_altv = 1
-    vim.g.netrw_winsize = 33
+    vim.g.netrw_winsize = 25
 
-    vim.cmd('colorscheme desert')
+    vim.cmd [[colorscheme slate]]
+    vim.opt.guifont = "CaskaydiaCove_Nerd_Font_Mono:h18"
     vim.opt.rtp:append('/opt/local/share/fzf/vim')
 
     -- mappings
     local opts = { noremap=true, silent=true }
+    vim.keymap.set("n", "<leader>y", "\"+y<CR>", opts)
+    vim.keymap.set("n", "<leader>Y", "\"+yg_<CR>", opts)
+    vim.keymap.set("n", "<leader>p", "\"+p<CR>", opts)
+    vim.keymap.set("n", "<leader>P", "\"+P<CR>", opts)
     vim.keymap.set("n", "<S-h>", ":bp<CR>", opts)
     vim.keymap.set("n", "<S-l>", ":bn<CR>", opts)
-    vim.keymap.set("n", "<C-e>", ":Lexplore<CR>", opts)
+    vim.keymap.set("n", "<C-p>", ":Files<CR>", opts)
+    vim.keymap.set("n", "<C-e>", ":Neotree<CR>", opts)
+    vim.keymap.set("n", "<leader>b", ":Neotree buffers<CR>", opts)
+    vim.keymap.set("n", "<leader>g", ":Neotree git_status<CR>", opts)
+
+    require("bufferline").setup{} 
 end
 
 load_plugins()
