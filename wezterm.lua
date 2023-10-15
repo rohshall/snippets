d1764e724eaebe2ade9a7aa60ca145b3a9a521b4
Salil Wadnerkar <rohshall@noreply.codeberg.org>
2023-10-15 18:46:32 +0000

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -2,20 +2,9 @@
 local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'Sweet Eliverlara (Gogh)'
-
-config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono'
+local config = wezterm.config_builder()
+config.color_scheme = 'Batman'
+config.font = wezterm.font 'Inconsolata Nerd Font Mono'
 config.font_size = 16.0
 
 -- and finally, return the configuration to wezterm
