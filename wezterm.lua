bc535b0b71b8a0e8d9883e31f7df98695aeda2a9
Salil Wadnerkar <rohshall@noreply.codeberg.org>
2023-12-02 18:36:33 +0000

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -3,9 +3,11 @@ local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
 local config = wezterm.config_builder()
-config.color_scheme = 'Banana Blueberry'
+
+-- This is where you actually apply your config choices
+config.color_scheme = 'synthwave-everything'
 config.font = wezterm.font 'Inconsolata Nerd Font Mono'
-config.font_size = 16.0
+config.font_size = 20.0
 
 -- and finally, return the configuration to wezterm
 return config
