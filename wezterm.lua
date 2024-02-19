36213b66bee9c08c5fc378b01f6edd2167b1606b
Salil Wadnerkar <rohshall@noreply.codeberg.org>
2024-02-19 05:16:16 +0000

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 -- config.color_scheme = 'Builtin Pastel Dark'
+-- config.color_scheme = 'Sonokai (Gogh)'
 config.color_scheme = 'Broadcast'
 config.font = wezterm.font 'Inconsolata Nerd Font Mono'
 config.font_size = 28.0
