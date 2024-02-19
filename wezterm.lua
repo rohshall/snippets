0d4f4b1b7fcb29a8ab1c3626c003c379c2a50535
Salil Wadnerkar <bsdprg@tuta.io>
2024-02-18 20:50:29 -0800

    Updated wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -5,9 +5,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'synthwave-everything'
+-- config.color_scheme = 'Builtin Pastel Dark'
+config.color_scheme = 'Broadcast'
 config.font = wezterm.font 'Inconsolata Nerd Font Mono'
-config.font_size = 20.0
+config.font_size = 28.0
 
 -- and finally, return the configuration to wezterm
 return config
