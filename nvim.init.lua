29c7e87a1dedd98671917d3a7eb0901d22cba660
Salil Wadnerkar <rohshall@noreply.codeberg.org>
2024-03-18 03:58:50 +0000

    Update nvim.init.lua

--- nvim.init.lua
+++ nvim.init.lua
@@ -1,3 +1,36 @@
+local plugins = {
+    'dense-analysis/ale',
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup{}
+        end,
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+}
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins)
+
 vim.opt.omnifunc = 'ale#completion#OmniFunc'
 vim.opt.termguicolors = true
 vim.opt.syntax = 'on'
@@ -27,7 +60,6 @@ vim.g.netrw_winsize = 25
 
 vim.cmd [[colorscheme slate]]
 vim.opt.guifont = "Inconsolata:h18"
--- vim.opt.rtp:append('/opt/local/share/fzf/vim')
 -- mappings
 local opts = { noremap=true, silent=true }
 vim.keymap.set("n", "<leader>x", "\"+x<CR>", opts)
@@ -35,57 +67,9 @@ vim.keymap.set("n", "<leader>y", "\"+y<CR>", opts)
 vim.keymap.set("n", "<leader>p", "\"+p<CR>", opts)
 vim.keymap.set("n", "<Tab>", ":bn<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bp<CR>", opts)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-
-local plugins = {
-    'dense-analysis/ale',
-    {
-        "junegunn/fzf",
-        keys = {
-            { "<C-p>", "<cmd>FZF<cr>", desc = "fzf" },
-        }
-    },
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require("bufferline").setup{}
-        end,
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    'tpope/vim-eunuch',
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-        },
-        keys = {
-            { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "Toggle" },
-            { "<C-e>", "<cmd>Neotree<cr>", desc = "NeoTree" },
-            { "<leader>b", "<cmd>Neotree buffers<cr>", desc = "Buffers" },
-            { "<leader>g", "<cmd>Neotree git_status<cr>", desc = "Git" },
-        },
-        config = function()
-            require("neo-tree").setup()
-        end,
-    }
-}
-
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup(plugins)
