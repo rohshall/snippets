b020f82d0e39ccaa524871659901104075a70b5b
rohshall <rohshall@noreply.codeberg.org>
2024-08-16 16:49:58 +0000

    Update wezterm.lua

--- wezterm-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cargo fetch --locked
-cargo build --release --frozen --manifest-path wezterm/Cargo.toml
-cargo build --release --frozen --manifest-path wezterm-gui/Cargo.toml
-cargo build --release --frozen --manifest-path wezterm-mux-server/Cargo.toml
-cargo build --release --frozen --manifest-path strip-ansi-escapes/Cargo.toml
-
-tic -x -o terminfo termwiz/data/wezterm.terminfo
-cargo test --frozen
--- wezterm.lua
+++ wezterm.lua
@@ -5,13 +5,13 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
--- config.color_scheme = 'flexoki-dark'
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'flexoki-dark'
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
-config.font_size = 16.0
+config.font_size = 12.0
 config.front_end = 'OpenGL'
 config.freetype_load_target = 'Light'
 config.freetype_render_target = 'HorizontalLcd'
+config.cell_width = 0.9
 
 -- and finally, return the configuration to wezterm
 return config
