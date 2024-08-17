aa81eab3c8d9dba67cea6be3e85f2abc16d5cd64
Salil Wadnerkar <salquestfl@icloud.com>
2024-08-17 12:41:11 -0700

    removed mini deps

--- nvim.init.lua
+++ nvim.init.lua
@@ -1,35 +1,43 @@
-local plugins = {
-    'dense-analysis/ale',
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require("bufferline").setup{}
-        end,
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-}
-
-
+-- lazy setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins)
+-- plug
+require 'lazy'.setup({
+	'neovim/nvim-lspconfig',
+	'dense-analysis/ale',
+	'nvim-tree/nvim-web-devicons',
+	'akinsho/bufferline.nvim',
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
+	{ 'ibhagwan/fzf-lua', branch = 'main' },
+	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+	'rebelot/kanagawa.nvim'
+})
+
+
+-- nvim-treesitter
+require 'nvim-treesitter.configs'.setup {
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false
+	}
+}
+
+-- mason and mason-lspconfig
+require 'mason'.setup()
+require 'mason-lspconfig'.setup()
 
 vim.opt.omnifunc = 'ale#completion#OmniFunc'
 vim.opt.termguicolors = true
@@ -48,28 +56,17 @@ vim.opt.linebreak = true
 vim.opt.list = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.modeline = false
+vim.opt.guifont = "FiraCode Nerd Font Mono:h14"
+-- colorscheme
+vim.cmd [[colorscheme kanagawa]]
+
 -- vim globals
 vim.g.ale_completion_enabled = 1
--- Keep the current directory and the browsing directory synced to avoid file move errors 
-vim.g.netrw_keepdir = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 4
-vim.g.netrw_altv = 1
-vim.g.netrw_winsize = 25
 
-vim.cmd [[colorscheme slate]]
-vim.opt.guifont = "Inconsolata:h18"
--- mappings
 local opts = { noremap=true, silent=true }
-vim.keymap.set("n", "<leader>x", "\"+x<CR>", opts)
-vim.keymap.set("n", "<leader>y", "\"+y<CR>", opts)
-vim.keymap.set("n", "<leader>p", "\"+p<CR>", opts)
+-- fzf
+vim.keymap.set('n', '<leader>f', "<CMD>lua require('fzf-lua').files()<CR>", opts)
+vim.keymap.set('n', '<leader>b', "<CMD>lua require('fzf-lua').buffers()<CR>", opts)
+-- buffers
 vim.keymap.set("n", "<Tab>", ":bn<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bp<CR>", opts)
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
